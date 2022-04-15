a7c54accafb274062578171890adcc7e3a147a29
Magesh <103715925+magesh-m-consultant@users.noreply.github.com>
2022-04-15 11:47:31 +0530

    Import Pandas changed location

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 
 import streamlit
-import pandas
+
 streamlit.title('my parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
@@ -8,5 +8,6 @@ streamlit.text('Kale, Spinach & Rocket Smothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe('my_fruit_list')
