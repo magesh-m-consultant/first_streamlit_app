9eb26b920bf69acc9665265862b3817450174c57
Magesh <103715925+magesh-m-consultant@users.noreply.github.com>
2022-04-15 11:09:37 +0530

    4 row Added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 
 import streamlit
 
+streamlit.title('my parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
