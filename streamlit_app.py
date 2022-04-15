210216a644211bf7fc82751c27b88f1518a8a66d
Magesh <103715925+magesh-m-consultant@users.noreply.github.com>
2022-04-15 12:11:54 +0530

    my_fruit_list.loc[fruits_selected]

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,5 +14,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 #let's put a pick list here so they can pick the fruit they want to include
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display the table on the page
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
+#display the table on the page
+#streamlit.dataframe(my_fruit_list)
