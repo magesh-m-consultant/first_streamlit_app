c5b56ab76dff28ec7c76da66b16174f27985678c
Magesh <103715925+magesh-m-consultant@users.noreply.github.com>
2022-04-17 14:49:03 +0530

    Fruityvice Fruit Advice!

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 #display the table on the page
 #streamlit.dataframe(my_fruit_list)
+streamlit.header('🍌🥭 Fruityvice Fruit Advice! 🥝🍇')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
