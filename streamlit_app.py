49fd26e17824c91a62b45d5a399ce3959022a79e
Magesh <103715925+magesh-m-consultant@users.noreply.github.com>
2022-04-19 21:25:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,5 +37,5 @@ fruit_choice =streamlit.text_input('What fruit would you like information about?
 streamlit.write('The User entered', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+
 
