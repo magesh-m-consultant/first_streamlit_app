758a0840156fcfb5c6c85335b637681ece052b9a
Magesh <103715925+magesh-m-consultant@users.noreply.github.com>
2022-04-20 17:11:43 +0530

    function

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,11 @@ streamlit.dataframe(fruits_to_show)
 
 
 
+#create the repeatable code block (called a function)
+def get_fruitvice_data(this_fruit_choice)
+    fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
 
 streamlit.header('Fruityvice Fruit Advice!')
 try:   
@@ -33,9 +38,11 @@ try:
    if not fruit_choice:
        streamlit.error("Please select a fruit to get information.")
    else:
-       fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-       fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
-       streamlit.dataframe(fruityvice_normalized)
+       #fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+       #fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+       back_from_function = get_fruitvicedata(fruit_choice)
+       #streamlit.dataframe(fruityvice_normalized)
+       streamlit.dataframe(back_from_function)
 
 except URLError as e:
     streamlit.error()
