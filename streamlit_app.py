d8a89767bda89d2be1c77955d1ab0ee13bea3f0d
Magesh <103715925+magesh-m-consultant@users.noreply.github.com>
2022-04-20 17:15:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,10 +37,8 @@ try:
    fruit_choice=streamlit.text_input('What fruit would you like information about?')
    if not fruit_choice:
        streamlit.error("Please select a fruit to get information.")
-   else:
-       #fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-       #fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
-       back_from_function = get_fruitvicedata(fruit_choice)
+   else:       
+       back_from_function = get_fruitvice_data(fruit_choice)
        #streamlit.dataframe(fruityvice_normalized)
        streamlit.dataframe(back_from_function)
 
