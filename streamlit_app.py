0bc8b58bc67df09e84b9cb79ad1e2ff18ef486bf
Magesh <103715925+magesh-m-consultant@users.noreply.github.com>
2022-04-20 17:25:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ streamlit.dataframe(my_data_rows)
 #streamlit.text(my_data_row)
 
 #Snowflake-related functions
-  def get_fruit_load_list();
+  def get_fruit_load_list():
     with my_cnx.cursor() as my_cur;
          my_cur.execute("select * from fruit_load_list")
          return my_cur.fetchall()
