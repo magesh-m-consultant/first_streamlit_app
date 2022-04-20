88b1a67d009863422e09415107927d0ed04d24bd
Magesh <103715925+magesh-m-consultant@users.noreply.github.com>
2022-04-20 16:19:51 +0530

    Execute

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,4 +58,5 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit=streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding', add_my_fruit)
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
