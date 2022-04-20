716e0657c00301409298f6647f90141e53ce94ff
Magesh <103715925+magesh-m-consultant@users.noreply.github.com>
2022-04-20 15:56:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,9 +40,11 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
-my_data_row = my_cur.fetchone()
+#my_data_row = my_cur.fetchone()
+my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+#streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
 #streamlit.text("The fruit load list contains:")
 #streamlit.text(my_data_row)
 
