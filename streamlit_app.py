eca70df749bb1dee3fc9bc45a757438e12596c6c
Magesh <103715925+magesh-m-consultant@users.noreply.github.com>
2022-04-20 17:25:20 +0530

    Button

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,7 +62,19 @@ streamlit.dataframe(my_data_rows)
 #streamlit.text("The fruit load list contains:")
 #streamlit.text(my_data_row)
 
-
+#Snowflake-related functions
+  def get_fruit_load_list();
+    with my_cnx.cursor() as my_cur;
+         my_cur.execute("select * from fruit_load_list")
+         return my_cur.fetchall()
+         
+        
+#add button to load the fruit
+   if streamlit.button('Get Fruit Load List'):
+        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+        my_data_rows = get_fruit_load_list()
+        streamlit.text(my_data_rows)
+        
 #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 #my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
