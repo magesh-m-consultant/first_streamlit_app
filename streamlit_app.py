6e5813d980b426433e40cc22ae9eadce158718a0
Magesh <103715925+magesh-m-consultant@users.noreply.github.com>
2022-04-20 17:31:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,22 +51,22 @@ except URLError as e:
 
 streamlit.stop()
 #import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("select * from fruit_load_list")
 #my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
+#my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 #streamlit.dataframe(my_data_row)
-streamlit.dataframe(my_data_rows)
+#streamlit.dataframe(my_data_rows)
 #streamlit.text("The fruit load list contains:")
 #streamlit.text(my_data_row)
 
 #Snowflake-related functions
   def get_fruit_load_list():
-      with my_cnx.cursor() as my_cur;
-           my_cur.execute("select * from fruit_load_list")
-           return my_cur.fetchall()
+    with my_cur.cursor() as my_cur:
+         my_cur.execute("select * from fruit_load_list")
+         return my_cur.fetchall()
          
         
 #add button to load the fruit
