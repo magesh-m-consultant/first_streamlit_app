7bf34fad6c8c3cb0c21c257741122edcd5f1eaba
Magesh <103715925+magesh-m-consultant@users.noreply.github.com>
2022-04-20 16:38:10 +0530

    Moved

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,16 +28,20 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice=streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered', fruit_choice)
+try:
+   fruit_choice=streamlit.text_input('What fruit would you like information about?')
+   if not fruit_choice:
+       streamlit.error("Please select a fruit to get information")
+   else
+     fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+     fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+     streamlit.dataframe(fruityvice_normalized)
+
+ expect URLError as e:
+     streamlit.error()
+    
 #import requests
-fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
-
-
-
-fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
 
 
 streamlit.stop()
