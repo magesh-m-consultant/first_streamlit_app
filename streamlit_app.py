ea75de3983d576fd8e8d581d128fc390a3e51453
Magesh <103715925+magesh-m-consultant@users.noreply.github.com>
2022-04-20 16:40:20 +0530

    moved 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,14 +28,14 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header('Fruityvice Fruit Advice!')
-try:
+try:   
    fruit_choice=streamlit.text_input('What fruit would you like information about?')
    if not fruit_choice:
-       streamlit.error("Please select a fruit to get information")
-   else
-     fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-     fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
-     streamlit.dataframe(fruityvice_normalized)
+       streamlit.error("Please select a fruit to get information.")
+   else:
+       fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+       fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+       streamlit.dataframe(fruityvice_normalized)
 
  expect URLError as e:
      streamlit.error()
